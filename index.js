ded9199deb9a245d571034b939bd4dabe0cd349a
Sarmila0 <sarmilarayamajhi01@gmail.com>
2022-06-08 10:58:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+console.log("hello")
+console.warn("goodmorning")
+console.error("everything is fine")
+
+var name='one';
+console.log(typeof(name))
+
+var ten='10';
+console.log(typeof(10))
+
+var number=1.1
+console.log(typeof(number))
+
+var age='55';
+console.log(typeof(age));
+
+var division='first';
+console.log(typeof(division));
+
+var number=[0,1,2,3];
+console.log(typeof(number));
+
+var y='55';
+console.log(y);
+
+var name='55';
+console.log(typeof(name));
+
+var quantity='100';
+console.log(quantity);
+
+var quality='19';
+console.log(parseInt(quality));
+
+var quantity='12.99';
+console.log(+quantity);
+
+var x='16';
+console.log(Number(x));
+
+var y=18;
+console.log(String(y));
+
+var q=true;
+console.log(Number(q),String(q));
+
+var t=true;
+console.log(Number(t),String(t));
+
+var a=1;
+console.log(a.toString());
+
+var y=68;
+console.log(y.toString());
+
+var k=55.55;
+console.log(parseInt(k));
+
+var p=33;
+console.log(p.toFixed(6));
+
+var d=0.12;
+console.log(parseFloat(d));
+
+var b=68;
+console.log(b.toFixed(4));
+
+var r=12+14;
+console.log(String(r))
+
+var f=46+2
+console.log(f.toString());
+
+
+
+const aa=10;
+
+{
+    let aa=20;
+    console.log(aa)
+}
+console.log(aa)
+
+const yy=40;
+
+{
+    let yy=50;
+    console.log(yy);
+}
+console.log(yy);
+
+const u=12+12;
+
+{
+    let u=22+22;
+    console.log(u)
+}
+console.log(u);
+
+const i=50-25;
+{
+    let i=44-40;
+    console.log(i);
+}
+console.log(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
